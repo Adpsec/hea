decf1089d55c50be96e6cfea4de0f7cc7084a0c0
Adpsec <asp.security@protonmail.com>
2023-10-13 10:41:13 -0400

    changes

--- app.py
+++ app.py
@@ -3,21 +3,9 @@ from pydantic import BaseModel
 import numpy as np
 from joblib import load
 import pathlib
-from fastapi.middleware.cors import CORSMiddleware
-
-
-origins = ["*"]
-
 
 app = FastAPI(title = 'Heart Disease Prediction')
 
-app.add_middleware(
-   CORSMiddleware,
-   allow_origins=origins,
-   allow_credentials=True,
-   allow_methods=["*"],
-   allow_headers=["*"]
-)
 model = load(pathlib.Path('model/heart-disease-v1.joblib'))
 
 class InputData(BaseModel):
@@ -42,5 +30,6 @@ class OutputData(BaseModel):
 def score(data:InputData):
     model_input = np.array([v for k,v in data.dict().items()]).reshape(1,-1)
     result = model.predict_proba(model_input)[:,-1]
-
+    
     return {'score':result}
+
